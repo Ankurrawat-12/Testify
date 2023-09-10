c23852f5b9bb7e1dedc81690cae39cfea62fdc57
Hariom8799 <hariomkumar01868@gmail.com>
2023-09-10 13:34:42 +0530

    check answer end points created

--- src/components/MCQ.tsx
+++ src/components/MCQ.tsx
@@ -5,7 +5,8 @@ import { ChevronRightIcon, Timer } from "lucide-react";
 import React from "react";
 import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
-
+import MCQCounter from "./MCQCounter";
+ 
 type Props = {
     game: Game & { questions: Pick<Question, "id" | "options" | "question">[] };
 };
@@ -18,6 +19,7 @@ const MCQ = ({ game }: Props) => {
         return game.questions[questionIndex];
     }, [questionIndex, game.questions]);
 
+
     const options = React.useMemo(() => {
         if (!currentQuestion) return [];
         if (!currentQuestion.options) return [];
@@ -27,17 +29,21 @@ const MCQ = ({ game }: Props) => {
     return (
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[80vw] max-w-4xl w-[90vw]">
             <div className="flex flex-row justify-between">
-                <p>
-                    <span className="text-slate-400 mr-2">Topic</span>
-                    <span className="px-2 py-1 text-white rounded-lg bg-slate-800">
-                        {game.topic}
-                    </span>
-                </p>
-                <div className="flex self-start mt-3 text-slate-400">
-                    <Timer className="mr-2" />
-                    <span>00:00</span>
+                <div className="flex flex-col">
+
+                
+                    <p>
+                        <span className="text-slate-400 mr-2">Topic</span>
+                        <span className="px-2 py-1 text-white rounded-lg bg-slate-800">
+                            {game.topic}
+                        </span>
+                    </p>
+                    <div className="flex self-start mt-3 text-slate-400">
+                        <Timer className="mr-2" />
+                        <span>00:00</span>
+                    </div>
                 </div>
-                {/* <MCQCounter /> */}
+                    <MCQCounter correctAnswers={3} wrongAnswers={3} />
             </div>
             <Card className="w-full mt-4">
                 <CardHeader className="flex flex-row items-center">
